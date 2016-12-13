444122a912a125547a3ec4dbe335f95d76a2df87
Wilson <wilsonliuxyz@gmail.com>
2016-12-13 17:33:02 +0800

    添加新博文

--- demo/PHP/part_one.php
+++ demo/PHP/part_one.php
@@ -5,3 +5,6 @@
     $fp = fopen("http://blog.wilsonliu.cn", "r");
     print_r(stream_get_meta_data($fp));
     fclose($fp);
+
+    wget https://github.com/downloads/libevent/libevent/libevent-2.0.15-stable.tar.gz
+    wget http://memcached.googlecode.com/files/memcached-1.4.9.tar.gz
